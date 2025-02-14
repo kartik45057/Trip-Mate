f5dd0d05733ca66b4f7b028dacaf9f4b4485826b
kartik <singhkartik45057@gmail.com>
2025-02-14 20:27:11 +0530

    Add files

---
+++ app/database/payment_crud.py
@@ -0,0 +1,72 @@
+from sqlmodel import Session, select, update
+from app.database.db_models import Expense, Payment, Trip, User
+from app.models import Expense_Create
+from app.database.db_main import engine
+from sqlalchemy.orm import selectinload
+        
+def get_payment_by_id_from_db(payment_id):
+    with Session(engine) as session:
+        try:
+            statement = select(Payment).where(Payment.id == payment_id).options(selectinload(Payment.expense))
+            result = session.exec(statement).first()
+            payment_part_of_expense = result.expense
+            return result
+        except Exception as e:
+            raise e
+
+def update_payment_details_in_db(payment_id, currency, amount, payment_mode, user_id):
+    with Session(engine) as session:
+        try:
+            if amount:
+                payment_detail = get_payment_by_id_from_db(payment_id)
+                if payment_detail:
+                    expense_id = payment_detail.expense_id
+                    expense_amount = payment_detail.expense.amount
+                    payment_amount = payment_detail.amount
+                    if amount > payment_amount:
+                        extra_amount = amount - payment_amount
+                        statement_to_update_expense_amount = update(Expense).where(Expense.id == expense_id).values(amount = expense_amount + extra_amount)
+                        session.exec(statement_to_update_expense_amount)
+                    elif amount < payment_amount:
+                        deducted_amount = payment_amount - amount
+                        statement_to_update_expense_amount = update(Expense).where(Expense.id == expense_id).values(amount = expense_amount - deducted_amount)
+                        session.exec(statement_to_update_expense_amount)
+                else:
+                    return {"message": "Item not found"}
+
+            query = update(Payment).where(Payment.id == payment_id)
+            if currency and amount and payment_mode and user_id:
+                statement = query.values(currency=currency, amount=amount, payment_mode=payment_mode, user_id=user_id)
+            elif currency and amount and payment_mode:
+                statement = query.values(currency=currency, amount=amount, payment_mode=payment_mode)
+            elif currency and amount and user_id:
+                statement = query.values(currency=currency, amount=amount, user_id=user_id)
+            elif amount and payment_mode and user_id:
+                statement = query.values(amount=amount, payment_mode=payment_mode, user_id=user_id)
+            elif currency and amount:
+                statement = query.values(currency=currency, amount=amount)
+            elif currency and payment_mode:
+                statement = query.values(currency=currency, payment_mode=payment_mode)
+            elif currency and user_id:
+                statement = query.values(currency=currency, user_id=user_id)
+            elif amount and payment_mode:
+                statement = query.values(amount=amount, payment_mode=payment_mode)
+            elif amount and user_id:
+                statement = query.values(amount=amount, user_id=user_id)
+            elif payment_mode and user_id:
+                statement = query.values(payment_mode=payment_mode, user_id=user_id)
+            elif currency:
+                statement = query.values(currency=currency)
+            elif amount:
+                statement = query.values(amount=amount)
+            elif payment_mode:
+                statement = query.values(payment_mode=payment_mode)
+            elif user_id:
+                statement = query.values(user_id=user_id)
+
+            session.exec(statement)
+            session.commit()
+            return {"message": "details updated successfully"}
+        except Exception as e:
+            session.rollback()
+            raise e
---
+++ app/routes/payment_route.py
@@ -0,0 +1,30 @@
+from typing import Optional
+from fastapi import APIRouter, HTTPException, Query, status
+from fastapi.params import Depends
+from app.database.payment_crud import get_payment_by_id_from_db, update_payment_details_in_db
+from app.enums import CurrencyCode, PaymentMode
+from app.models import Payment_Read, User_Read
+from util.auth import get_current_user
+
+admin_user_email = "adminUser@gmail.com"
+
+router = APIRouter()
+    
+@router.get("/payment/{payment_id}", status_code=status.HTTP_200_OK, response_model=Payment_Read)
+def get_payment_by_id(payment_id: int, current_user: User_Read = Depends(get_current_user)):
+    try:
+        result = get_payment_by_id_from_db(payment_id)
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"An error occurred: {str(e)}")
+    
+    if not result:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Item not Found")
+    return result
+ 
+@router.put("/payment", status_code=status.HTTP_200_OK)
+def update_payment_details(payment_id: int, currency: Optional[CurrencyCode] = Query(None), amount: Optional[float] = Query(None), payment_mode: Optional[PaymentMode] = Query(None), user_id: Optional[int] = Query(None), current_user: User_Read = Depends(get_current_user)):
+    try:
+        result = update_payment_details_in_db(payment_id, currency, amount, payment_mode, user_id)
+        return result
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"An error occurred: {str(e)}")
